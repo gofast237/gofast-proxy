b3977a91b1bda9321eab9db8ff7db04d7e229e07
jeff <jeff>
2025-03-08 17:17:01 +0100

    proxy

--- index.js
+++ index.js
@@ -3,23 +3,28 @@ const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-
 app.use(cors({ origin: true }));
 
 const firebaseProxy = createProxyMiddleware({
   target: 'https://gofast-835a5-default-rtdb.firebaseio.com',
   changeOrigin: true,
-  ws: true
+  ws: true,
+  secure: true,
+  pathRewrite: {
+    '^/': '/', // Laissez inchangé, mais important pour clarté
+  },
+  logLevel: 'debug', // pour observer clairement ce qu'il se passe
+  headers: {
+    "Connection": "keep-alive"
+  }
 });
 
 app.use('/', firebaseProxy);
 
 const PORT = process.env.PORT || 4000;
 
-// ⚠️ Important : stockez "app.listen" dans une variable "server"
 const server = app.listen(PORT, () => {
   console.log(`🚀 Proxy Firebase fonctionnel sur le port ${PORT}`);
 });
 
-// ✅ Gérez explicitement l'événement websocket upgrade
 server.on('upgrade', firebaseProxy.upgrade);
